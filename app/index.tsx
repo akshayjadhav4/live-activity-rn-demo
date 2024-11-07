ec71bb120f2f8e246d11bc4a1fd14f9ca3b73cc9
Akshay Jadhav <akshayjadhav461999@gmail.com>
2024-11-07 19:02:32 +0530

    Basic UI to test live activity

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,46 @@
-import { Text, View } from "react-native";
-
+import { Button, Text, View } from "react-native";
+import {
+  areActivitiesEnabled,
+  endActivity,
+  startActivity,
+  updateActivity,
+  PackageStatus,
+} from "local:order-tracking";
+import { useState } from "react";
 export default function Index() {
+  const [currentStatus, setCurrentStatus] = useState<PackageStatus>(
+    PackageStatus.Shipped
+  );
+
+  function updateStatus(_currentStatus: string) {
+    switch (_currentStatus) {
+      case PackageStatus.Shipped:
+        updateActivity({
+          packageStatus: PackageStatus.InTransit,
+          estimatedDeliveryTime: Math.floor(Date.now() / 1000),
+        });
+        setCurrentStatus(PackageStatus.InTransit);
+        break;
+      case PackageStatus.InTransit:
+        updateActivity({
+          packageStatus: PackageStatus.OutForDelivery,
+          estimatedDeliveryTime: Math.floor(Date.now() / 1000),
+        });
+
+        setCurrentStatus(PackageStatus.OutForDelivery);
+        break;
+      case PackageStatus.OutForDelivery:
+        endActivity({
+          packageStatus: PackageStatus.Delivered,
+          estimatedDeliveryTime: Math.floor(Date.now() / 1000),
+        });
+        setCurrentStatus(PackageStatus.Delivered);
+        break;
+      default:
+        setCurrentStatus(PackageStatus.Delivered);
+        break;
+    }
+  }
   return (
     <View
       style={{
@@ -9,7 +49,25 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>iOS Live Activity</Text>
+
+      <Button
+        title="Start"
+        onPress={() => {
+          if (areActivitiesEnabled()) {
+            startActivity({
+              carrierName: "Fast Shipping Co.",
+              trackingNumber: "1Z9999",
+              packageStatus: PackageStatus.Shipped,
+              estimatedDeliveryTime: Math.floor(Date.now() / 1000),
+            });
+          }
+        }}
+      />
+      <Button
+        title={`Update PackageStatus`}
+        onPress={() => updateStatus(currentStatus)}
+      />
     </View>
   );
 }
